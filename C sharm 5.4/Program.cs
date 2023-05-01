18d594c8e29c434f684bd3fc043c90c332b9d285
Daveabi7 <rrrrabigor@gmail.com>
2023-05-01 17:15:44 +0300

    Create Program.cs

---
+++ C sharm 5.4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPrime(int n)
+    {
+        if (n < 2)
+        {
+            return false;
+        }
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 99; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+
